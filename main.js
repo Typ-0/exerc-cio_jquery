aeea63a75315a65ca40e741c97f56069f387ccff
Victor Cavalcanti <v.cavalcanti.gsilva@gmail.com>
2023-12-21 06:19:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#formulario').submit(function (event) {
+        event.preventDefault(); 
+        var tarefa = $('#inputTarefa').val();
+        $('#listaTarefas').append('<li>' + tarefa + '</li>');
+        $('#inputTarefa').val('');
+    });
+});
+$(document).ready(function () {
+    $(document).on('click', '#listaTarefas li', function () {
+        $(this).toggleClass('concluida');
+    });
+});
